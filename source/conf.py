16c1d7d92379b9f82a98241524190ea3921d966d
drisley0302 <46612503+drisley0302@users.noreply.github.com>
2019-05-06 23:01:10 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -22,8 +22,8 @@ language = None
 pygments_style = 'sphinx'
 
 
-#html_theme = 'pylons'
-html_theme = 'pylons'
+#html_theme = 'classic'
+html_theme = 'classic'
 
 rst_epilog = """
 .. include:: substitutions.txt
